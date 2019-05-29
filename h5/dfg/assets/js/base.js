84c24ed2fe1b161dd0ee2bc3a0186d0841e630f3
王风坤 <hqwfk@126.com>
2019-05-29 19:47:41 +0800

    Merge branch 'master' of https://github.com/wfk2022612/wfk2022612.github.io
    # Conflicts:
    #	h5/yjdd/cn/static/img/上滑提示.935382c.gif
    #	h5/yjdd/cn/static/js/manifest.3ad1d5771e9b13dbdad2.js.map
    #	h5/yjdd/index.html
    #	h5/yjdd/static/img/AjOMa3pH.935382c.gif
    #	h5/yjdd/static/img/上滑提示.935382c.gif
    #	static/media/bg.39678e0.mp3

---
+++ h5/dfg/assets/js/base.js
@@ -0,0 +1,160 @@
+var imgs = {
+  play: './assets/images/play.png',
+  logo: './assets/images/logo2.png',
+  indexBg: './assets/images/index-bg1.jpg',
+  introduceBg: './assets/images/introduce-bg.jpg',
+  detailBg: './assets/images/detail-bg.jpg',
+  teamBg: './assets/images/team-bg1.jpg',
+  introTitle: './assets/images/intro-title.png',
+  //friendTitle: './assets/images/friend-title.jpg',
+  detailTitle1: './assets/images/detail-title1.png',
+  detailTitle2: './assets/images/detail-title2.png',
+  detailTitle3: './assets/images/detail-title3.png',
+  entry: './assets/images/entry.png',
+  arrow: './assets/images/arrow.png',
+  like: './assets/images/like.png',
+  point: './assets/images/point.png',
+  back: './assets/images/back.png',
+  //index: './assets/images/index.png',
+  //videoPoster: "./assets/images/video-poster.png",
+  //mask: './assets/images/mask.png',
+
+  msg: './assets/images/msg1.png',
+  title1: './assets/images/title1-2.png',
+  c11: './assets/images/c11.jpg',
+  c21: './assets/images/c21.jpg',
+  c31: './assets/images/c31.jpg',
+  
+  heart: './assets/images/heart.jpg',
+
+  head: './assets/images/head.png',
+  edit: './assets/images/edit.png',
+
+}
+
+var texts = {
+  zlmIntro: '2019年5月22日，中央宣传部授予杜富国同志“时代楷模”荣誉称号。',
+  zlmName: '杜富国：为人民扫雷，为军旗增辉',
+  zlmIntroContent: [
+    '杜富国是陆军某扫雷排爆大队战士。入伍以来，他始终把忠诚和信仰刻在心中，把使命和责任扛在肩上，主动请缨征战雷场，苦练精练过硬本领，为人民利益和边境安宁挥洒热血，在平凡岗位干出了突出业绩。',
+    '2018年10月11日，杜富国随队参加排雷作业时，危急时刻冲锋在前，为保护战友身受重伤，失去双眼和双手。杜富国同志曾先后获得“全国自强模范”“感动中国十大人物”、陆军“四有”新时代革命军人标兵等称号，荣立个人一等功。'
+  ],
+}
+
+var aigangjingye = {//爱岗敬业
+  title: imgs.detailTitle1,
+  //videoPoster: imgs.videoPoster,
+  //videoUrl: './assets/video/5M.mp4',
+  content: [
+    '2018年10月11日下午，杜富国所在的扫雷大队4队在云南边境老山主峰西侧的雷场进行作业。组长杜富国和战友艾岩为一个作业组，他们是生死相依的战友加兄弟。在坡顶扫排爆破筒“翻犁”（爆破）过的土地上，他们发现了一枚露出部分弹体的爆炸物。杜富国初步判断，这是一颗当量大、危险性高的加重手榴弹。根据以往经验，下面可能埋藏着一个雷窝。',
+    {img:imgs.c11,title:'杜富国（右）与战友艾岩进行搜排作业（2018年4月9日摄）。'},
+    '他们迅速报告了情况。分队长张波当即命令杜富国“查明有无诡计设置”。面对这颗疑点重重的爆炸物，作为组长的杜富国命令艾岩“你退后，让我来”。',
+    '按照作业规程，杜富国开始小心翼翼清除弹体周围浮土。突然“轰”的一声巨响，杜富国下意识地倒向了艾岩一侧。飞来的弹片伴随着强烈的冲击波，把他身上的防护服瞬间炸成了棉絮状，杜富国顿时倒在血泊之中，当场失去了双手和双眼。也正是杜富国这舍生忘死的刹那一挡，两三米外的艾岩，只受了些皮外伤。',
+  ]
+}
+var yongyuchuangxin = { //勇于创新
+  title: imgs.detailTitle2,
+  content: [
+    '两个手掌当场被炸飞，双眼球破裂内容物溢出，右眼球造成脱落，大腿根部至面部创伤面积达90%以上……从鬼门关冲出来的杜富国，恢复知觉后第一句话是：“艾岩怎样？”提的第一个要求是：“赶紧治好我的伤，我还要去扫雷！”',
+    {img:imgs.c21,title:'杜富国（左二）在家人和战友的陪伴下进行下肢力量康复训练（2019年4月15日摄）。'},
+    '在陆军军医大学西南医院康复中心，杜富国的坚强毅力，折服了为他疗伤的所有医护人员：“我们当医生的，从来没有见过这么坚强的战士!”每天，杜富国的双眼、浑身的伤疤，都需要清洗换药。撕裂般的疼痛，让常人难以忍受。开始时，护士余翔总是轻轻地为他作处理。“护士，你不要怕。我不怕疼！只要让我功能恢复，早日回到部队，再大的苦我也能吃！”',
+    '恢复知觉后的一段日子里，浑身缠满绷带的杜富国只知道伤很重，并不知道失去了双手、双眼。他对陪护的战友说：“我想多吃点肉、多喝点牛奶，这样手上多长点肉，就能早日回部队，又可以一起扫雷了！”',
+    '如何把真实伤情告知杜富国，部队领导和专家为他制定了多套心理干预方案。然而，这些方案一套也没用上。得知真实伤情，杜富国稍作沉默，反而安慰起领导和医生：“我知道了，你们放心吧，我会坚强起来的！我不能扫雷了，但我还可以给人们讲扫雷的故事。”'
+  ]
+}
+
+var ganyufengxian = { //甘于奉献
+  title: imgs.detailTitle3,
+  content: [
+    '杜富国刚到扫雷大队时，由于文化基础薄弱，首次理论摸底考试都没有及格。一向不服输的他把排雷理论知识点制成小卡片，有空就拿出来背。几本扫雷教材都被他翻得卷了边，里面满是圈圈点点的红笔标注。查看杜富国的理论考试成绩表，从不及格到90分，他用了45天；临战训练综合性考核所有课目被评为全优。杜富国说，只有刻苦训练搜排技能，才能完成扫雷任务，才能与死神擦肩而过。',
+    {img:imgs.c13,title:'在云南麻栗坡，杜富国展示自己排除的1枚地雷（2016年10月24日摄）。'},
+    '探雷器是扫雷兵的“手中枪”，“学会5分钟，学精要5年”。为把探雷器练成“第三只手”，他将铁钉、石块、弹片等混合埋设，并使用斜放、混合、覆盖等方法增加难度，3个月的训练期还没结束，他就熟练掌握了10余种地雷的排除方法，做到探雷器一探就能准确辨别埋设位置、判明种类，分辨出是金属残片还是雷管引信。',
+    '“他有‘金刚钻’，敢揽‘瓷器活’！”扫雷大队政委周文春说，“杜富国勤奋钻研，他敢说‘让我来’，是因为有本事、有底气！”'
+  ]
+}
+//【舍生忘死】【乐观坚强】【苦练技能】
+var indexMenu = [
+  { name: '舍生忘死', value: 'aigangjingye' },
+  { name: '乐观坚强', value: 'yongyuchuangxin' },
+  { name: '苦练技能', value: 'ganyufengxian' },
+]
+
+var comments = [
+  {
+    headimgurl: imgs.head,
+    name: '',
+    content: '',
+  },
+  {
+    headimgurl: imgs.head,
+    name: '',
+    content: ''
+  }
+
+
+]
+
+
+
+var arr = [];
+
+
+for (var attr in imgs) {
+  arr.push(imgs[attr]);
+}
+
+
+var mainImgList = [
+  {
+    url: imgs.img1,
+    type: 'rect'
+  }, {
+    url: imgs.img2,
+    type: 'circle'
+  }, {
+    url: imgs.img3,
+    type: 'circle'
+  }
+];
+
+var musics = {
+  music: {
+    src: './assets/music/bg2.mp3',
+    autoplay: false,
+    name: 'bg',
+    loop: true
+  },
+  photo: {
+    src: './assets/music/photo.mp3',
+    autoplay: false,
+    name: "photo",
+    loop: false
+  }
+};
+
+(function () {
+
+  if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
+    handleFontSize();
+  } else {
+    if (document.addEventListener) {
+      document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+    } else if (document.attachEvent) {
+      document.attachEvent("WeixinJSBridgeReady", handleFontSize);
+      document.attachEvent("onWeixinJSBridgeReady", handleFontSize);
+    }
+  }
+
+  function handleFontSize() {
+    // 设置网页字体为默认大小
+    WeixinJSBridge.invoke('setFontSizeCallback', {
+      'fontSize': 0
+    });
+    // 重写设置网页字体大小的事件
+    WeixinJSBridge.on('menu:setfont', function () {
+      WeixinJSBridge.invoke('setFontSizeCallback', {
+        'fontSize': 0
+      });
+    });
+  }
+})();
---
+++ h5/dfg/assets/js/data.js
@@ -0,0 +1,7 @@
+var headimgList = [];
+
+for(var i =0 ;i<100;i++){
+    headimgList.push({
+        headimgurl:'./assets/images/'+(i+1)+'.jpg'
+    })
+}
